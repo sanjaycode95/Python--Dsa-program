a1cbfe07ab8d689cdfb4211e26a06b2f24192f99
sanjaycode95 <san9629678023@gmail.com>
2025-07-22 21:17:14 +0530

    updated

---
+++ Linked_list/delet_element.py
@@ -0,0 +1,33 @@
+class store:
+    def __init__(self):
+        self.element=None
+        self.node=None    
+class linked_list:
+    def __init__(self):
+        self.head=None
+        self.current=None
+    def add(self,a):
+        address=store()
+        address.element=a
+        if self.head==None:            
+            self.current=address
+            self.head=address
+        else:
+            self.current.node=address
+            self.current=address
+    def delet(self,a):
+             b=self.head
+             if b.element==a:
+                 self.head=b.node
+                 return None
+             while b:
+                 if a==b.node.element:
+                     b.node=b.node.node
+                     break
+                 b=b.node
+a=linked_list()
+a.add(1)
+a.add(2)
+a.add(3)
+a.add(4)
+a.delet(4)
---
+++ Linked_list/reverse_linked_list.py
@@ -0,0 +1,40 @@
+class store:
+    def __init__(self):
+        self.element=None
+        self.node=None    
+class linked_list:
+    def __init__(self):
+        self.head=None
+        self.current=None
+    def add(self,a):
+        address=store()
+        address.element=a
+        if self.head==None:            
+            self.current=address
+            self.head=address
+        else:
+            self.current.node=address
+            self.current=address
+    def display(self):
+         b=self.head
+         while b:
+             print(b.element,end="->")
+             b=b.node
+    def rev(self):
+        pre=None
+        curr=self.head
+        while curr:
+            dump=curr.node
+            curr.node=pre
+            pre=curr
+            curr=dump
+        self.head=pre 
+a=linked_list()
+a.add(1)
+a.add(2)
+a.add(3)
+a.add(4)
+a.display()
+a.rev()
+print("\n\treversed_list")
+a.display()
